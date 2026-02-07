29f40c64fdf702cfab7997ac8102d819ecc8954b
shannon-bit <shannon@autonomizehq.com>
2026-02-07 08:10:07 -0700

    Add files via upload

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,6 @@
 """
 RGSS Phoenix v4 Core
+
+This package implements the canonical logic rules defined in docs/LOGIC_RULES.md.
+No I/O is performed in this package. Adapters (Sheets/Watson/etc.) must live elsewhere.
 """
